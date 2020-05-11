823ffd30ec8b883c30f0ce50d6a41ce745986256
aakashGoel94 <akashonnet@gmail.com>
2020-05-11 13:56:01 +0200

    alpha 0.0.1

--- scptest.c
+++ scptest.c
@@ -10,12 +10,10 @@ int scp_read(ssh_session session);
 int scp_helloworld(ssh_session session, ssh_scp scp);
 int scp_receive(ssh_session session, ssh_scp scp);
 
-
 int main()
 {
   ssh_session my_ssh_session;
   int verbosity = SSH_LOG_PROTOCOL;
-  //int port = 22;
   int rc;
   char *password;
  
@@ -25,7 +23,6 @@ int main()
     exit(-1);
   ssh_options_set(my_ssh_session, SSH_OPTIONS_HOST, "localhost");
   ssh_options_set(my_ssh_session, SSH_OPTIONS_LOG_VERBOSITY, &verbosity);
-  //ssh_options_set(my_ssh_session, SSH_OPTIONS_PORT, &port);
   
 
   // Connect to server
@@ -65,8 +62,6 @@ int main()
   ssh_disconnect(my_ssh_session);
   ssh_free(my_ssh_session);
 }
-
-
 
 int verify_knownhost(ssh_session session)
 {
@@ -154,58 +149,6 @@ int verify_knownhost(ssh_session session)
     ssh_clean_pubkey_hash(&hash);
     return 0;
 }
-
-// int show_remote_processes(ssh_session session)
-// {
-//   ssh_channel channel;
-//   int rc;
-//   char buffer[256];
-//   int nbytes;
- 
-//   channel = ssh_channel_new(session);
-//   if (channel == NULL)
-//     return SSH_ERROR;
- 
-//   rc = ssh_channel_open_session(channel);
-//   if (rc != SSH_OK)
-//   {
-//     ssh_channel_free(channel);
-//     return rc;
-//   }
- 
-//   rc = ssh_channel_request_exec(channel, "ps aux");
-//   if (rc != SSH_OK)
-//   {
-//     ssh_channel_close(channel);
-//     ssh_channel_free(channel);
-//     return rc;
-//   }
- 
-//   nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
-//   while (nbytes > 0)
-//   {
-//     if (write(1, buffer, nbytes) != (unsigned int) nbytes)
-//     {
-//       ssh_channel_close(channel);
-//       ssh_channel_free(channel);
-//       return SSH_ERROR;
-//     }
-//     nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
-//   }
- 
-//   if (nbytes < 0)
-//   {
-//     ssh_channel_close(channel);
-//     ssh_channel_free(channel);
-//     return SSH_ERROR;
-//   }
- 
-//   ssh_channel_send_eof(channel);
-//   ssh_channel_close(channel);
-//   ssh_channel_free(channel);
- 
-//   return SSH_OK;
-// }
 
 int scp_write(ssh_session session)
 {
@@ -297,7 +240,6 @@ int scp_read(ssh_session session)
   ssh_scp_free(scp);
   return SSH_OK;
 }
-
 
 int scp_receive(ssh_session session, ssh_scp scp)
 {
